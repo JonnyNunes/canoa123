281f71ac7b54652cf57e22fff7f6ab3bf777c084
Jonny <nunes.jonny1_1>
2023-05-15 20:59:57 -0400

    Projeto enviado

---
+++ src/main/java/esbam/canoa123/model/Cidades.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package esbam.canoa123.model;
+
+/**
+ *
+ * @author ACER
+ */
+public class Cidades {
+    
+    /// ATRIBUTOS DA CLASSE
+    private int id;
+    private String localidade;
+    private String estado;
+}
